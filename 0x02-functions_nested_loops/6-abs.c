56b7fbbd952cd20cad11a983d6fca180a975480e
Olayemi Peter Pelumi <peterpelumi9@gmail.com>
2022-09-13 03:05:54 -0700

    hh

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute value of an integer
+ * @ab: integer input
+ * Return: absolute value of ab
+ */
+int _abs(int ab)
+{
+return (ab * ((as > 0) - (as < 0)));
+}

ec5525089992baf758577ddff8b7a1fe482229be
Olayemi Peter Pelumi <peterpelumi9@gmail.com>
2022-09-13 15:09:24 -0700

    hj

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,24 +2,11 @@
 /**
  * _isalpha - a function that checks for alphabetic character
  * @c: single letter input
- * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
+ * Return: 1 if r is a letter (lower or uppercase), 0 otherwise
  */
-int _isalpha(int c)
+int _isalpha(int r)
 {
-if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-return (1);
-else
-return (0);
-}
-#include "main.h"
- /**
-  * _isalpha - a function that checks for alphabetic character
-  * @c: single letter input
-  * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
-  */
-int _isalpha(int c)
-{
-if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+  if (((r >= 'a') && (r <= 'z')) || ((r >= 'A') && (r <= 'Z')))
 return (1);
 else
 return (0);

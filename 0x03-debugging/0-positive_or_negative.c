f1d56df0694eebbd1349143968328a7c44744aac
Olayemi Peter Pelumi <peterpelumi9@gmail.com>
2022-09-16 01:55:29 -0700

    exclude

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -10,20 +10,20 @@
  */
 int main(void)
 {
-int n;
+int i;
 srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n < 0)
+i = rand() - RAND_MAX / 2;
+if (i < 0)
 {
-printf("%d is negative\n", n);
+printf("%d is negative\n", i);
 }
-else if (n == 0)
+else if (i == 0)
 {
-printf("%d is zero\n", n);
+printf("%d is zero\n", i);
 }
 else
 {
-printf("%d is positive\n", n);
+printf("%d is positive\n", i);
 }
 return (0);
 }
